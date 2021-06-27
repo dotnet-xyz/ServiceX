69303bd8be30b617a191397e3128b58c1b1fa959
Sergey Somkov <sergey@somkov.com>
2021-06-27 07:53:44 +0000

    #8 - build log output cleanup

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ set -u
 
 POSTGRES_VERSION=13.3
 
+# Separate quiet pull for clean build logs.
+docker pull --quiet ${POSTGRES_VERSION}
+
 docker run \
 	--name=postgres \
-	--tty \
 	-d --rm \
 	-e "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" \
 	--network=host \
@@ -19,11 +21,15 @@ docker run \
 
 # docker build
 
+# Separate quiet pull for clean build logs.
+docker pull --quiet mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+docker pull --quiet mcr.microsoft.com/dotnet/sdk:5.0-alpine
+
 docker build \
 	--network=host \
 	--build-arg NUGET_API_KEY=${NUGET_API_KEY} \
 	--build-arg NUGET_SOURCE=${NUGET_SOURCE} \
 	--build-arg SERVICE_REPOSITORY_URL=${SERVICE_REPOSITORY_URL} \
 	--build-arg SERVICE_VERSION=${SERVICE_VERSION} \
-	-t dotnet-xyz/service-x:${SERVICE_VERSION} \
+	-t ${SERVICE_IMAGE_NAME}:${SERVICE_VERSION} \
 	.
